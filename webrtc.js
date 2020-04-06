cfecd40e4782f53089416141993c196d05d51b4d
yrz <214691+yrz@users.noreply.github.com>
2020-04-06 11:25:52 +0200

    revamp log from webrtc

--- webrtc.js
+++ webrtc.js
@@ -2,29 +2,30 @@ async function create_offer(pc, user) {
     log('initiating '+user);
     await pc.setLocalDescription(await pc.createOffer());
     pc.onicecandidate = ({candidate}) => {
+      if (candidate) log("OnICECandidate (ao)"+candidate.address);
       if (candidate) return;
-      log("LOCAL DESCRIPTION");
-      log(pc.localDescription.sdp)
+      //null candidate means I've got them all
       ws_send_sdp(user, 'offer', pc.localDescription.sdp);
     };
   }
   
   async function accept_offer(pc, user, offer_value) {
     log('accept offer '+user);
-    log(offer_value);
     if (pc.signalingState != "stable") return;
     await pc.setRemoteDescription({type: "offer", sdp: offer_value});
     await pc.setLocalDescription(await pc.createAnswer());
     pc.onicecandidate = ({candidate}) => {
+      if (candidate) log("OnICECandidate (ao)"+candidate.address);
       if (candidate) return;
-      log("LOCAL DESCRIPTION");
-      log(pc.localDescription.sdp)
+      //null candidate means I've got them all
       ws_send_sdp(user, 'answer', pc.localDescription.sdp);
     };
+    status_log += 'L1 '+user+' offer accepted\r\n';
   };
   
   function accept_answer(pc, answer_value) {
     if (pc.signalingState != "have-local-offer") return;
     pc.setRemoteDescription({type: "answer", sdp: answer_value});
+    status_log += 'L1 answer accepted\r\n';
   };
   
